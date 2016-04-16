1e07a7dfe3cec9c1b1187f6863776dfe18110b80
Rot256 <Rot256@users.noreply.github.com>
2016-04-16 15:19:00 +0100

    Fuck me

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db, login_manager
 from sqlalchemy import func
 from app.models import User
