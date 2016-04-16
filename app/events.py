b60240fb61e6de59ff904609800030c564edd80d
Rot256 <Rot256@users.noreply.github.com>
2016-04-16 23:24:12 +0100

    Added cleanup code

--- app/events.py
+++ app/events.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from app.ai import AI1
 from app.game import BlackHoleGame
 from flask.ext.login import login_required, current_user
@@ -8,64 +9,85 @@ from binascii import hexlify
 
 open_games = {}
 running_games = {}
+user_games = {}
+
+game_lock = threading.Lock()
+
+def clear_user(user):
+    try:
+        game = user_games[user]
+    except KeyError:
+        return
+    leave_room(game.room)
+    emit('left', {}, room=game.room)
+    del user_games[user]
 
 @socketio.on('join')
 @login_required
 def on_join(data):
     # Send userid back to user
     emit('join', {
-        'user': current_user.id
+        'player': current_user.id
     })
 
-    # Check if AI game
-    if 'mode' in data and data['mode'] == 'AI':
-        game = BlackHoleGame(AI=AI1)
-        game.player = [current_user.id]
-        emit('play', game.to_json(), room=game.room)
+    # Lock game table
+    game_lock.acquire()
+    clear_user(current_user.id)
 
-    # LOCK : TODO
-    for room in open_games:
-        # Join an existing game
-        game = open_games[room]
-        if current_user.id in game.players:
-            continue
-        game.players.append(current_user.id)
-        running_games[room] = game
-        del open_games[room]
-        join_room(game.room)
-        emit('play', game.to_json(), room=game.room)
-        break
-    else:
-        # Create a new game
-        game = BlackHoleGame()
-        game.players.append(current_user.id)
+    if 'mode' in data and data['mode'] == 'AI':
+        # Create AI game
+        game = BlackHoleGame(AI=AI1())
+        game.players = [current_user.id, None]
         open_games[game.room] = game
         join_room(game.room)
-
+        game.play_AI()
+        emit('play', game.to_json(), room=game.room)
+        app.logger.info('Created AI game for: %d' % game.players[0])
+    else:
+        # Create human games
+        for room in open_games:
+            # Join an existing game
+            game = open_games[room]
+            if current_user.id in game.players:
+                continue
+            game.players.append(current_user.id)
+            running_games[room] = game
+            del open_games[room]
+            join_room(game.room)
+            emit('play', game.to_json(), room=game.room)
+            app.logger.info('Paired user: %d with %d' % (game.players[0], game.players[1]))
+            break
+        else:
+            # Create a new game
+            game = BlackHoleGame()
+            game.players.append(current_user.id)
+            open_games[game.room] = game
+            join_room(game.room)
+            app.logger.info('Cleared a new game lobby for: %d' % game.players[0])
+    game_lock.release()
 
 @socketio.on('play')
 @login_required
 def handle_play_event(data):
-    print(data)
+    # Validate input
     try:
         game = running_games[data['room']]
         tile = int(data['tile'])
     except (KeyError, ValueError):
-        print("ERROR")
+        app.logger.error('Client send a play move, the server did not understand')
         return 'HUGE ERROR'
 
-    print(tile)
-    print(current_user.id)
+    # Play move
     if game.play(tile, current_user.id):
-        print('Look who wins:', game.winner)
-        print("emit play")
+        app.logger.info('User %d just played' % current_user.id)
+        if game.AI:
+            app.logger.info('AI is making a move against %d' % current_user.id)
+            game.play_AI()
         emit('play', game.to_json(), room=game.room)
 
 
 @socketio.on('disconnect')
 @login_required
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    # send(username + ' has left the room.', room=room)
+def handle_disconnect_event(data):
+    app.logger.info('Client %d has disconnected' % current_user.id)
+    clear_user(current_user.id)
