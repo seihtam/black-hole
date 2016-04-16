e774cff1b165079270662f2c44ae99ed4fac052c
Rot256 <Rot256@users.noreply.github.com>
2016-04-16 17:01:04 +0100

    Socketio event server

--- app/game.py
+++ app/game.py
@@ -1,19 +1,39 @@
+import os
+from binascii import hexlify
 
 class BlackHoleGame():
     def __init__(self, AI_game = False):
+        self.room = hexlify(os.urandom(16))
         self.AI_game = AI_game
-        self.board = {n: None for n in range(21)}
+        self.board = {n: None for n in range(1, 22)}
         self.turn = 0
         self.last_move = 0  # Time of last move
-        self.player1 = None # User object
-        self.player2 = None # User object
-        self.room1 = None   # Room Id of player1
-        self.room2 = None   #
+        self.players = []   # User ids
+        self.current = 0    # Current player
         pass
+
+    def play(self, n, player):
+        if self.board[n] != None:
+            return False
+        if self.players[self.current] != player:
+            return False
+        if n > 21 or n < 1 :
+            return False
+
+        # Do it
+        self.board[n] = {
+            'room': self.room,
+            'player': player,
+            'value': self.turn // 2,
+        }
+        self.turn += 1
+        self.current = (self.current + 1) % len(self.players)
+        return True
 
     def to_json(self, player):
         return {
             'board': self.board,
-            'brick': self.brick,
+            'players': self.players,
+            'current': self.players[self.current],
         }
 
