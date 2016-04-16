e61a13729525105377c1c95866bd6dc4effb7b2c
Rot256 <Rot256@users.noreply.github.com>
2016-04-16 20:13:54 +0100

    Fixed starter game

--- app/game.py
+++ app/game.py
@@ -1,15 +1,21 @@
 import os
 from binascii import hexlify
 
+board_graph = {
+    1: [2, 3],
+
+}
+
 class BlackHoleGame():
     def __init__(self, AI_game = False):
         self.room = hexlify(os.urandom(16)).decode('utf-8')
         self.AI_game = AI_game
         self.board = {n: None for n in range(1, 22)}
-        self.turn = 0
-        self.last_move = 0  # Time of last move
-        self.players = []   # User ids
-        self.current = 0    # Current player
+        self.turn = 0                         # Current turn (for both sides)
+        self.last_move = 0                    # Time of last move
+        self.players = []                     # User ids
+        self.current = ord(os.urandom(1)) % 2 # Current player
+        self.started = self.current
 
     def play(self, n, player):
         if self.board[n] != None:
@@ -34,11 +40,17 @@ class BlackHoleGame():
                 return self.play(tile, player)
         pass
 
+    def get_neightbors(self, n):
+        pass
+
+
     def to_json(self):
         return {
             'room': self.room,
             'board': self.board,
             'players': self.players,
+            'started': self.started,
             'current_player': self.players[self.current],
         }
 
+BlackHoleGame()
