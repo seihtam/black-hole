931c2f9128216f05b47e5cdc7d6e09ba496e22de
synchr <synchr@users.noreply.github.com>
2016-04-18 11:39:17 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import create_app, socketio
 from werkzeug import SharedDataMiddleware
 
 if __name__ == '__main__':
-    app = create_app('sqlite:///app.db', debug=True)
+    app = create_app('sqlite:///app.db', debug=False)
     app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
         '/': os.path.join(os.path.dirname(__file__), 'app/static'),
     })
